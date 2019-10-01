c5ffa2ff9b29ee4463f28530393480de0ba3cc46
Everton Melo <evertonsquaq@gmail.com>
2019-10-01 19:57:30 -0300

    added an auth form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import Layout from './hoc/Layout/Layout'
-import BurguerBuilder from './containers/BurgerBuilder/BurguerBuilder'
-import Orders from './containers/Orders/Orders'
+import Layout from './hoc/Layout/Layout';
+import BurguerBuilder from './containers/BurgerBuilder/BurguerBuilder';
+import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 import Checkout from './containers/Checkout/Checkout';
 import {Route, Switch} from 'react-router-dom';
 
@@ -15,6 +16,7 @@ class App extends Component {
           <Switch>
             <Route path="/checkout" component={Checkout}/>
             <Route path="/orders" component={Orders}/>
+            <Route path="/auth" component={Auth}/>
             <Route path="/" exact component={BurguerBuilder}/>
             <Route/>
           </Switch>
